f78a5d99b0817b15c973e9c43fb52f127b33e252
Taskeen Fatima <81077704+Tessfatima7@users.noreply.github.com>
2021-03-29 03:50:32 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "-" << setw(61) << setfill('-') << "-" <<endl;
+    cout << "|" << setw(10) << setfill(' ') << "Name" << setw (7) << setfill(' ') ;
+    cout << "|" << setw(18) << setfill(' ') << "Enrollment No" << setw (4) << setfill(' ');
+    cout << "|" << setw (5) << setfill(' ') << "Age" <<setw (2) << setfill (' ');
+    cout << "|" << setw (9) << setfill(' ') << "Marks" << setw (6) << setfill(' ') << "|"<< endl;
+    cout << "-" << setw(61) << setfill('-') << "-" <<endl;
+
+
+    cout << "|" << setw(10) << setfill(' ') << "Taskeen" << setw (7) << setfill(' ') ;
+    cout << "|" << setw(18) << setfill(' ') << "03-134211-045" << setw (4) << setfill(' ');
+    cout << "|" << setw (5) << setfill(' ') << "18" <<setw (2) << setfill (' ');
+    cout << "|" << setw (9) << setfill(' ') << "97/100" << setw (6) << setfill(' ') << "|"<< endl;
+
+
+    cout << "|" << setw(10) << setfill(' ') << "Asim" << setw (7) << setfill(' ') ;
+    cout << "|" << setw(18) << setfill(' ') << "03-134211-006" << setw (4) << setfill(' ');
+    cout << "|" << setw (5) << setfill(' ') << "19" <<setw (2) << setfill (' ');
+    cout << "|" << setw (9) << setfill(' ') << "98/100" << setw (6) << setfill(' ') << "|"<< endl;
+
+    cout << "|" << setw(10) << setfill(' ') << "Huzama" << setw (7) << setfill(' ') ;
+    cout << "|" << setw(18) << setfill(' ') << "03-134211-013" << setw (4) << setfill(' ');
+    cout << "|" << setw (5) << setfill(' ') << "19" <<setw (2) << setfill (' ');
+    cout << "|" << setw (9) << setfill(' ') << "97/100" << setw (6) << setfill(' ') << "|"<< endl;
+
+    cout << "|" << setw(10) << setfill(' ') << "Khalid" << setw (7) << setfill(' ') ;
+    cout << "|" << setw(18) << setfill(' ') << "03-134211-016" << setw (4) << setfill(' ');
+    cout << "|" << setw (5) << setfill(' ') << "18" <<setw (2) << setfill (' ');
+    cout << "|" << setw (9) << setfill(' ') << "99/100" << setw (6) << setfill(' ') << "|"<< endl;
+
+    cout << "-" << setw(61) << setfill('-') << "-" <<endl;
+    return 0;
+}
